542bfba9f6c44df881391c0904c9ff7f21058f02
Vitalii-tmp <vitaliilchuk6@gmail.com>
2021-02-09 12:52:40 +0200

    step 1 update

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -3,28 +3,28 @@
 
 class Player {
 private:
-	std::string Id, Name, Rank;
+	std::string Id, Name, Rank; //Id, Name, Rank гравця
 public:
 };
 
 class PlayerManager {
-	void CreatePlayer(std::string args)
+	void CreatePlayer(std::string args)       //створити гравця
 	{
 
 	}
-	void GetPlayerByName(std::string args)
+	void GetPlayerByName(std::string args)	  //показати гравця по імені
 	{
 
 	}
-	void GetPlayerById(std::string args)
+	void GetPlayerById(std::string args) //показати гравця по ід
 	{
 
 	}
-	void DeletePlayer(std::string args) 
+	void DeletePlayer(std::string args) // видалити гравця
 	{
 
 	}
-	void ShowPlayerInfo(std::string args)
+	void ShowPlayerInfo(std::string args) // інф про гравця
 	{
 
 	}
@@ -32,28 +32,28 @@ class PlayerManager {
 
 class Hero {
 private:
-	std::string Id, Name, HP, Damage;
+	std::string Id, Name, HP, Damage;  //Id, Name, HP, Damage героя
 public:
 };
 
 class HeroManager {
-	void CreateHero(std::string args)
+	void CreateHero(std::string args)     //створити героя
 	{
 
 	}
-	void GetHeroByName(std::string args)
+	void GetHeroByName(std::string args)  //показати героя по імені
 	{
 
 	}
-	void GetHeroById(std::string args)
+	void GetHeroById(std::string args)   //показати героя по ід
 	{
 
 	}
-	void DeleteHero(std::string args)
+	void DeleteHero(std::string args)    //видалити героя
 	{
 
 	}
-	void ShowHeroInfo(std::string args)
+	void ShowHeroInfo(std::string args)  //інф про героя
 	{
 
 	}
@@ -65,19 +65,19 @@ class Team {
 };
 
 class TeamManager {
-	void GenerateNewTeam(std::string args)
+	void GenerateNewTeam(std::string args)     //створити команду
 	{
 
 	}
-	void GetTeamInfo(std::string args)
+	void GetTeamInfo(std::string args)        //отримати інформацію про команду 
 	{
 
 	}
 };
 
 class Session {
-	int StartTime, TeamOne, TeamTwo, Winner;
-	void CalculateWinner(std::string args) 
+	int StartTime, TeamOne, TeamTwo, Winner;      //час гри, рахунок кожної команди
+	void CalculateWinner(std::string args)        //рахує переможця
 	{
 
 	}
@@ -85,7 +85,7 @@ class Session {
 
 class GameManager {
 	
-	void PerformGameSession() 
+	void PerformGameSession()                //створює ігрову сесію
 	{
 
 	};
